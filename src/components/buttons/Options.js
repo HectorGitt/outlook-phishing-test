c1822c2b393056db6c2dda950f776478282e108e
HectorGitt <adeniyi.olaitanhector@yahoo.com>
2022-05-18 18:42:37 +0100

    made buttons centralize and responsive

--- src/components/buttons/Options.js
+++ src/components/buttons/Options.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { Row, Button, Col } from 'react-bootstrap'
 const Options = ({handleCorrect}) => {
   return (
-    <Row className="w-50 mx-auto align-items-center gy-3 text-center">
-        <Col xs={12} lg={6}>
+    <Row className="gy-3 mx-auto text-center text-lg-start align-items-center">
+        <Col xs={12} lg={{span:3, offset:3}}>
             <Button onClick={handleCorrect} className="answer" variant="light" as="input" type="button" value="Phishing" />{' '}
         </Col>
-        <Col xs={12} lg={6} >
+        <Col xs={12} lg={{span:6, offset:0}} >
             <Button onClick={handleCorrect} className="answer" variant="light" as="input" type="button" value="Legitimate" />{' '}
         </Col>
     </Row>
